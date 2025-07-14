60d92c8887f9293d87a3ac11ea947f9be32fd689
SaeedAchiever <123863213+SaeedAchiever@users.noreply.github.com>
2025-07-14 03:00:16 +0000

    Add files via upload

--- Components/MatchMe/MatchForm.js
+++ Components/MatchMe/MatchForm.js
@@ -1,62 +1,61 @@
-import { View, Text, ScrollView, TextInput, Modal,Image } from 'react-native'
-import React, { useState } from 'react'
+import {
+  View,
+  Text,
+  ScrollView,
+  TextInput,
+  Modal,
+  Image,
+  Pressable,
+} from "react-native";
+import React, { useState } from "react";
 
-import Universities from '../University/universities.json'
+import Universities from "../University/universities.json";
 
+import styles from "./MatchStyle";
+import MatchUniversities from "./MatchUniversities";
 
-import styles from './MatchStyle'
-import MatchUniversities from './MatchUniversities'
-
-const Close = require('../../assets/close.png')
-
+const Close = require("../../assets/close.png");
 
 const MatchForm = () => {
+  const [isUniVisible, setIsUniVisible] = useState(false);
 
-const [isUniVisible, setIsUniVisible] = useState(false)
+  const [searchedData, setSearchedData] = useState("");
 
-const [searchedData, setSearchedData] = useState('');
-
-
-const searchedList = () => {
-  if (searchedData === '') {
-    return [];
-  }
-  return Universities.filter(
-    value => value.name.toLowerCase().startsWith(searchedData.toLowerCase())
-  );
-};
-
-
-
+  const searchedList = () => {
+    if (searchedData === "") {
+      return [];
+    }
+    return Universities.filter((value) =>
+      value.name.toLowerCase().startsWith(searchedData.toLowerCase())
+    );
+  };
 
   return (
     <View style={styles.MatchHomeMainContainer}>
-
-
       <ScrollView>
-      <View style={styles.MatchHomeHeadContainer}>
-        <Text style={styles.MatchHomeHeadText}>
-          Complete the form below to get recommended to 
-          Courses you're eligible to offer in your dream University
-        </Text>
-      </View>
-        
+        <View style={styles.MatchHomeHeadContainer}>
+          <Text style={styles.MatchHomeHeadText}>
+            Complete the form below to get recommended to Courses you're
+            eligible to offer in your dream University
+          </Text>
+        </View>
+
         <View style={styles.MatchHomeCourseContainer}>
           <Text style={styles.MatchHomeCourseText}>
             Which University would you love to attend
           </Text>
-            <TextInput 
-              style={styles.courseInput}
-              placeholder='Start typing then choose'
-              value={searchedData}
-            />
-            <View style={styles.matchDisclaimer}>
-              <Text style={styles.matchDisclaimerTitle}>Disclaimer</Text>
-              <Text style={styles.matchDisclaimerText}>
-                We do not store your results, its only used 
-                as you intend and discarded aterwards
-              </Text>
-            </View>
+          <TextInput
+            style={styles.courseInput}
+            placeholder="Start typing then choose"
+            value={searchedData}
+          />
+          <View style={styles.matchDisclaimer}>
+            <Text style={styles.matchDisclaimerTitle}>Disclaimer</Text>
+            <Text style={styles.matchDisclaimerText}>
+              We do not store your results, its only used as you intend and
+              discarded aterwards
+            </Text>
+          </View>
         </View>
 
         {/* Core */}
@@ -69,10 +68,7 @@ const searchedList = () => {
               <Text style={styles.coreMainText}>Mathematics</Text>
             </View>
             <View>
-              <TextInput  
-                placeholder='.....'
-                style={styles.gradeText}
-              />
+              <TextInput placeholder="....." style={styles.gradeText} />
             </View>
           </View>
 
@@ -81,10 +77,7 @@ const searchedList = () => {
               <Text style={styles.coreMainText}>English Lang.</Text>
             </View>
             <View>
-              <TextInput  
-                placeholder='.....'
-                style={styles.gradeText}
-              />
+              <TextInput placeholder="....." style={styles.gradeText} />
             </View>
           </View>
 
@@ -93,10 +86,7 @@ const searchedList = () => {
               <Text style={styles.coreMainText}>Science</Text>
             </View>
             <View>
-              <TextInput  
-                placeholder='.....'
-                style={styles.gradeText}
-              />
+              <TextInput placeholder="....." style={styles.gradeText} />
             </View>
           </View>
 
@@ -105,108 +95,87 @@ const searchedList = () => {
               <Text style={styles.coreMainText}>Social Studies</Text>
             </View>
             <View>
-              <TextInput  
-                placeholder='.....'
-                style={styles.gradeText}
-              />
+              <TextInput placeholder="....." style={styles.gradeText} />
             </View>
           </View>
-
         </View>
 
         {/* Electives */}
 
         <View style={styles.coreContainer}>
           <Text style={styles.coreText}>Elective Subjects</Text>
-          <Text style={styles.electiveText}>Enter your best three elective belows</Text>
+          <Text style={styles.electiveText}>
+            Enter your best three elective belows
+          </Text>
 
           <View style={styles.coreTextMainContainer}>
             <View style={styles.coreTextContainer}>
-            <TextInput  
+              <TextInput
                 style={styles.coreMainText}
-                placeholder='Type to choose'
+                placeholder="Type to choose"
               />
             </View>
             <View>
-              <TextInput  
-                placeholder='.....'
-                style={styles.gradeText}
-              />
+              <TextInput placeholder="....." style={styles.gradeText} />
             </View>
           </View>
 
           <View style={styles.coreTextMainContainer}>
             <View style={styles.coreTextContainer}>
-            <TextInput  
+              <TextInput
                 style={styles.coreMainText}
-                placeholder='Type to choose'
+                placeholder="Type to choose"
               />
             </View>
             <View>
-              <TextInput  
-                placeholder='.....'
-                style={styles.gradeText}
-              />
+              <TextInput placeholder="....." style={styles.gradeText} />
             </View>
           </View>
 
           <View style={styles.coreTextMainContainer}>
             <View style={styles.coreTextContainer}>
-            <TextInput  
+              <TextInput
                 style={styles.coreMainText}
-                placeholder='Type to choose'
+                placeholder="Type to choose"
               />
             </View>
             <View>
-              <TextInput  
-                placeholder='.....'
-                style={styles.gradeText}
-              />
+              <TextInput placeholder="....." style={styles.gradeText} />
             </View>
           </View>
-
         </View>
 
-        <View 
+        <Pressable
           style={styles.matchButton}
-          onTouchEnd={()=>{
-            setIsUniVisible(true)
+          onPress={() => {
+            setIsUniVisible(true);
           }}
         >
           <Text style={styles.matchButtonText}>Match Me</Text>
-        </View>
-
+        </Pressable>
       </ScrollView>
 
-      <Modal
-       visible={isUniVisible}
-       animationType='slide'
-      >
-      <View
-       style={{
-        position:'absolute',
-        right:20,top:20,
-        zIndex:100
-      }}
-      onTouchEnd={()=>{
-        setIsUniVisible(false)
-      }}
-      >
-        <Image 
-          source={Close}
-          style={{width:35,height:35}}
-        />
-      </View>
+      <Modal visible={isUniVisible} animationType="slide">
+        <Pressable
+          style={{
+            position: "absolute",
+            right: 20,
+            top: 20,
+            zIndex: 100,
+          }}
+          onPress={() => {
+            setIsUniVisible(false);
+          }}
+        >
+          <Image source={Close} style={{ width: 35, height: 35 }} />
+        </Pressable>
 
         <View>
-          <MatchUniversities  />
+          <MatchUniversities />
         </View>
-        
       </Modal>
-
-
     </View>
-  )
-}
+  );
+};
 
-export default MatchForm
+export default MatchForm;
--- Components/MatchMe/MatchUniversities.js
+++ Components/MatchMe/MatchUniversities.js
@@ -1,116 +1,129 @@
-import { View, Text,Image, FlatList } from 'react-native'
-import React from 'react'
+import { View, Text, Image, FlatList, Pressable } from "react-native";
+import React from "react";
 
-import universities  from  '../University/universities.json'
+import universities from "../University/universities.json";
 
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation } from "@react-navigation/native";
 
+const Location = require("../../assets/location.png");
+const Star = require("../../assets/star.png");
 
-const Location = require('../../assets/location.png')
-const Star = require('../../assets/star.png')
-
-import styles from './MatchStyle'
+import styles from "./MatchStyle";
 
 const MatchUniversities = () => {
-
-const navigation = useNavigation()
+  const navigation = useNavigation();
 
   return (
     <View style={styles.matchUniversitiesContainer}>
-
-      
-
-      <FlatList 
+      <FlatList
         data={universities}
-        renderItem={({item})=>{
-          return  (
-            <View
-            style={styles.FilterPageUniContainer}
-            onTouchEnd={()=>{
-             navigation.navigate('UniData', {university:  universities.find(
-              uni => uni.name === item.name
-             )})
-            }}
+        renderItem={({ item }) => {
+          return (
+            <Pressable
+              style={styles.FilterPageUniContainer}
+              onPress={() => {
+                navigation.navigate("UniData", {
+                  university: universities.find(
+                    (uni) => uni.name === item.name
+                  ),
+                });
+              }}
             >
-            <View style={styles.FilterPageUniHeaderContainer}>
-     
-             <View style={styles.FilterPageUniHeaderNameContainer}>
-              <Text style={styles.FilterPageUniHeaderNameText}>{`${item.name}`}</Text>
-             </View>
-     
-             <View style={styles.FilterBothContainer}>
-     
-              <View style={styles.FilterPageUniHeaderLocationContainer}>
-               <Image 
-                source={Location}
-                style={styles.FilterPageUniHeaderLocationImage}
-               />
-             <Text style={styles.FilterPageUniHeaderLocationText}>{`${item.location}`}</Text>
-              </View>
-              <View style={styles.FilterPageUniHeaderRatingContainer}>
-                <View style={styles.FilterPageUniHeaderRatingImageContainer}>
-                <Image 
-                  source={Star}
-                  style={styles.FilterPageUniHeaderLocationImage}
-                 />
-                 <Image 
-                  source={Star}
-                  style={styles.FilterPageUniHeaderLocationImage}
-                 />
-                 <Image 
-                  source={Star}
-                  style={styles.FilterPageUniHeaderLocationImage}
-                 />
-                 <Image 
-                  source={Star}
-                  style={styles.FilterPageUniHeaderLocationImage}
-                 />
-                 <Image 
-                  source={Star}
-                  style={styles.FilterPageUniHeaderLocationImage}
-                 />
+              <View style={styles.FilterPageUniHeaderContainer}>
+                <View style={styles.FilterPageUniHeaderNameContainer}>
+                  <Text
+                    style={styles.FilterPageUniHeaderNameText}
+                  >{`${item.name}`}</Text>
                 </View>
-                
-                <Text style={styles.FilterPageUniHeaderRateText}>{`4.5 Stars`}</Text>
+
+                <View style={styles.FilterBothContainer}>
+                  <View style={styles.FilterPageUniHeaderLocationContainer}>
+                    <Image
+                      source={Location}
+                      style={styles.FilterPageUniHeaderLocationImage}
+                    />
+                    <Text
+                      style={styles.FilterPageUniHeaderLocationText}
+                    >{`${item.location}`}</Text>
+                  </View>
+                  <View style={styles.FilterPageUniHeaderRatingContainer}>
+                    <View
+                      style={styles.FilterPageUniHeaderRatingImageContainer}
+                    >
+                      <Image
+                        source={Star}
+                        style={styles.FilterPageUniHeaderLocationImage}
+                      />
+                      <Image
+                        source={Star}
+                        style={styles.FilterPageUniHeaderLocationImage}
+                      />
+                      <Image
+                        source={Star}
+                        style={styles.FilterPageUniHeaderLocationImage}
+                      />
+                      <Image
+                        source={Star}
+                        style={styles.FilterPageUniHeaderLocationImage}
+                      />
+                      <Image
+                        source={Star}
+                        style={styles.FilterPageUniHeaderLocationImage}
+                      />
+                    </View>
+
+                    <Text
+                      style={styles.FilterPageUniHeaderRateText}
+                    >{`4.5 Stars`}</Text>
+                  </View>
+
+                  <View>
+                    <Text
+                      style={styles.FilterPageUniHeaderRateText}
+                    >{`${item.type} Inst.`}</Text>
+                  </View>
+                </View>
+
+                <View style={styles.FilterCourseFeesMainContainer}>
+                  <View style={styles.FilterCourseFeesContainer}>
+                    <Text style={styles.FilterCourseFeesHeadText}>
+                      Courses Offered
+                    </Text>
+                    <View>
+                      <Text
+                        style={styles.FilterCourseFeesText}
+                      >{`5 Courses`}</Text>
+                    </View>
+                  </View>
+                  <View style={styles.FilterCourseFeesContainer}>
+                    <Text style={styles.FilterCourseFeesHeadText}>
+                      Fee Range
+                    </Text>
+                    <View>
+                      <Text
+                        style={styles.FilterCourseFeesText}
+                      >{`GHC 1.7k-19.7k`}</Text>
+                    </View>
+                  </View>
+                </View>
+
+                <View style={styles.FilterAdmissionContainer}>
+                  <View style={styles.FilterCourseFeesContainer}>
+                    <Text style={styles.FilterCourseFeesHeadText}>
+                      Admission Status :{" "}
+                    </Text>
+                  </View>
+                  <View>
+                    <Text style={styles.FilterCourseFeesText}>Closed</Text>
+                  </View>
+                </View>
               </View>
-     
-              <View>
-               <Text style={styles.FilterPageUniHeaderRateText}>{`${item.type} Inst.`}</Text>
-              </View>
-              
-             </View>
-     
-             <View style={styles.FilterCourseFeesMainContainer}>
-              <View style={styles.FilterCourseFeesContainer}>
-               <Text style={styles.FilterCourseFeesHeadText}>Courses Offered</Text>
-               <View>
-               <Text style={styles.FilterCourseFeesText}>{`5 Courses`}</Text>
-               </View>
-              </View>
-              <View style={styles.FilterCourseFeesContainer}>
-               <Text style={styles.FilterCourseFeesHeadText}>Fee Range</Text>
-               <View>
-               <Text style={styles.FilterCourseFeesText}>{`GHC 1.7k-19.7k`}</Text>
-               </View>
-              </View>
-             </View>
-     
-             <View style={styles.FilterAdmissionContainer}>
-              <View style={styles.FilterCourseFeesContainer}>
-               <Text style={styles.FilterCourseFeesHeadText}>Admission Status : </Text>
-              </View>
-               <View>
-               <Text style={styles.FilterCourseFeesText}>Closed</Text>
-               </View>
-             </View>
-             
-            </View>
-           </View>
-          )
+            </Pressable>
+          );
         }}
       />
     </View>
-  )
-}
+  );
+};
 
-export default MatchUniversities
+export default MatchUniversities;
